32d6b282bac9a92bb337be0ce0299b7edfd51db6
Leila Skinner <beileiwang@gmail.com>
2020-10-09 17:06:22 -0500

    added tool bar icno buttons and styled them

--- src/components/TaskBar/TaskBar.tsx
+++ src/components/TaskBar/TaskBar.tsx
@@ -1,20 +1,39 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPen, faCheck, faTrash } from "@fortawesome/free-solid-svg-icons";
+import styled from "styled-components";
+
+const ToolBar = styled.div`
+  border: 1px solid gray;
+  border-radius: 3px;
+`;
+
+const ToolButton = styled.button`
+  background: none;
+  cursor: pointer;
+  font-size: 0.7em;
+  outline: 0;
+  padding: 2px 4px;
+  border: 0;
+
+  &:hover {
+    color: #758bfd;
+  }
+`;
 
 const TaskBar = () => {
   return (
-    <>
-      <button>
+    <ToolBar>
+      <ToolButton>
         <FontAwesomeIcon icon={faPen} />
-      </button>
-      <button>
+      </ToolButton>
+      <ToolButton>
         <FontAwesomeIcon icon={faCheck} />
-      </button>
-      <button>
+      </ToolButton>
+      <ToolButton>
         <FontAwesomeIcon icon={faTrash} />
-      </button>
-    </>
+      </ToolButton>
+    </ToolBar>
   );
 };
 
