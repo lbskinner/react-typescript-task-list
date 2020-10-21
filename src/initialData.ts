73ebf4699fbae566a91690a3244e3c99c73d368e
Leila Skinner <beileiwang@gmail.com>
2020-10-21 16:20:03 -0500

    update initial data per review

--- src/initialData.ts
+++ src/initialData.ts
@@ -2,7 +2,8 @@ const initialData: InitialData = {
   tasks: {
     "task-1": {
       id: "task-1",
-      content: "Hover over ☰ next to Task App on nav bar to add new column",
+      content:
+        "Hover over ☰ at the top left of the screen on nav bar to add new column",
       complete: false,
     },
     "task-2": {
@@ -12,7 +13,7 @@ const initialData: InitialData = {
     },
     "task-3": {
       id: "task-3",
-      content: "Hover over individual task to edit or delete task",
+      content: "Hover over individual task to view more options",
       complete: false,
     },
     "task-4": {
@@ -21,7 +22,7 @@ const initialData: InitialData = {
       complete: false,
     },
     "task-5": { id: "task-5", content: "Do dishes", complete: false },
-    "task-6": { id: "task-6", content: "Tak out trash", complete: false },
+    "task-6": { id: "task-6", content: "Take out trash", complete: false },
     "task-7": { id: "task-7", content: "Make dinner", complete: false },
     "task-8": { id: "task-8", content: "Pay bills", complete: false },
     "task-9": {
