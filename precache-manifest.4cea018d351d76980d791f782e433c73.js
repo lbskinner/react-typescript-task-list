15f9d312123b402f21350a8e2d5eba4de4d9e3fd
Leila Skinner <beileiwang@gmail.com>
2020-11-24 14:44:55 -0600

    Updates

---
+++ precache-manifest.4cea018d351d76980d791f782e433c73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b296bad16a9f3b1e32f090097198ab5d",
+    "url": "/react-typescript-task-list/index.html"
+  },
+  {
+    "revision": "8290c626ddd2338309f7",
+    "url": "/react-typescript-task-list/static/css/main.501ddd0b.chunk.css"
+  },
+  {
+    "revision": "ab2480d4ed3d62162213",
+    "url": "/react-typescript-task-list/static/js/2.b571bea5.chunk.js"
+  },
+  {
+    "revision": "a3c66bc87fa121efed0250fdac2dfa2b",
+    "url": "/react-typescript-task-list/static/js/2.b571bea5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8290c626ddd2338309f7",
+    "url": "/react-typescript-task-list/static/js/main.a2f0174b.chunk.js"
+  },
+  {
+    "revision": "ce26671e6e8ce85110e4",
+    "url": "/react-typescript-task-list/static/js/runtime-main.c395a1ea.js"
+  }
+]);
